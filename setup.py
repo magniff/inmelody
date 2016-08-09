223f468b23fc0bf3e3e8989ee0cc5965589c6373
magniff <tinysnippets@gmail.com>
2016-08-08 20:54:23 -0400

    Initial commit here.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='vktunes',
+    author='magniff',
+    version='0.0.1',
+    install_requires=["urwid", "vk", "python-vlc", "pyyaml", "click"],
+    url='https://github.com/magniff/vktunes',
+    description='Simple console vk music player.',
+)
